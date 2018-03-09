29e9282ec5b26414838a2d692d06fae62d4e9096
Timo Hedke <lucky_expert@t-online.de>
2018-03-09 21:26:18 +0100

    Moved SafeMAth out of Bllitz.sol.

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -2,6 +2,8 @@ var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
 var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
 var Authentication = artifacts.require("./Authentication.sol");
 var Blitz = artifacts.require("./Blitz.sol");
+var SafeMath = artifacts.require("./zeppelin/math/SafeMath.sol");
+
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
@@ -9,6 +11,9 @@ module.exports = function(deployer) {
   deployer.deploy(Killable);
   deployer.link(Killable, Authentication);
   deployer.deploy(Authentication);
+  //deployer.autolink();
+  deployer.deploy(SafeMath);
+  deployer.link(SafeMath, Blitz);
   deployer.deploy(Blitz);
 
 };
