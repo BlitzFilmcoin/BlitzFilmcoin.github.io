8da462056a7c1d62e94bb6cc7d6528dfc670d894
daan <daanlockhorst@hotmail.com>
2018-03-13 15:27:42 +0100

    added discountCrowdsale function

--- truffle/contracts/BlitzCrowdsale.sol
+++ truffle/contracts/BlitzCrowdsale.sol
@@ -2,10 +2,11 @@ pragma solidity ^0.4.18;
 
 import "./zeppelin2/crowdsale/validation/CappedCrowdsale.sol";
 import "./zeppelin2/crowdsale/validation/TimedCrowdsale.sol";
-//import "./zeppelin2/crowdsale/validation/WhitelistedCrowdsale.sol";
+import "./zeppelin2/crowdsale/validation/WhitelistedCrowdsale.sol";
 import "./zeppelin2/crowdsale/distribution/RefundableCrowdsale.sol";
 import "./zeppelin2/crowdsale/emission/MintedCrowdsale.sol";
 import "./zeppelin2/token/ERC20/MintableToken.sol";
+
 
 
 /**
@@ -19,7 +20,7 @@ import "./zeppelin2/token/ERC20/MintableToken.sol";
  * After adding multiple features it's good practice to run integration tests
  * to ensure that subcontracts works together as intended.
  */
-contract BlitzCrowdsale is CappedCrowdsale, RefundableCrowdsale, MintedCrowdsale {
+contract BlitzCrowdsale is CappedCrowdsale, RefundableCrowdsale, MintedCrowdsale, WhitelistedCrowdsale {
 
     function BlitzCrowdsale(uint256 _openingTime, uint256 _closingTime, uint256 _rate, 
                             address _wallet, uint256 _cap, MintableToken _token, uint256 _goal) public
@@ -27,10 +28,25 @@ contract BlitzCrowdsale is CappedCrowdsale, RefundableCrowdsale, MintedCrowdsale
         CappedCrowdsale(_cap)
         TimedCrowdsale(_openingTime, _closingTime)
         RefundableCrowdsale(_goal)
-        //WhitelistedCrowdsale()
+        WhitelistedCrowdsale()
+        
     {
         //As goal needs to be met for a successful crowdsale
         //the value needs to less or equal than a cap which is limit for accepted funds
         require(_goal <= _cap);
-    }
-}
+            
+        }
+    
+          
+    function softcapDiscount(address _beneficiary, uint256 _weiAmount) internal {
+
+        if (!goalReached()) {
+            _weiAmount = _weiAmount.mul(12).div(10);
+        }
+        
+        super._preValidatePurchase(_beneficiary, _weiAmount);
+        require(weiRaised.add(_weiAmount) <= cap);
+     }
+
+   }
+
