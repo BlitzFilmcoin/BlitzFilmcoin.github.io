44fdf6566e818700a60849a1a76858a776f6b005
Timo Hedke <lucky_expert@t-online.de>
2018-03-21 11:04:53 +0100

    adding token

--- truffle/src/containers/App.js
+++ truffle/src/containers/App.js
@@ -4,7 +4,9 @@ import { connect } from "react-redux";
 import {
   getCrowdsaleStats,
   instantiateCrowdsaleContract,
-  getNewStats
+  getNewStats,
+  getTokenStats,
+  instantiateTokenContract
 } from "../core/action";
 import getWeb3 from "../core/util/web3/getWeb3";
 
@@ -15,7 +17,9 @@ class App extends Component {
       .then(async results => {
         console.log("Web3 initialized!");
         await this.props.instantiateCrowdsaleContract();
+        await this.props.instantiateTokenContract()
         await this.props.getCrowdsaleStats();
+        await this.props.getTokenStats();
         await this.props.getNewStats();
       })
       .catch(err => {
@@ -45,5 +49,7 @@ const mapStateToProps = (state, ownProps) => {
 export default connect(mapStateToProps, {
   getCrowdsaleStats,
   instantiateCrowdsaleContract,
-  getNewStats
+  instantiateTokenContract,
+  getNewStats,
+  getTokenStats
 })(App);
--- truffle/src/core/action.js
+++ truffle/src/core/action.js
@@ -1,25 +1,101 @@
 import BlitzCrowdsaleContract from "../../build/contracts/BlitzCrowdsale.json";
+import BlitzCrowdsaleTokenContract from "../../build/contracts/BlitzCrowdsaleToken.json";
+
 const contract = require("truffle-contract");
 
 export const CROWDSALE_STATS_PENDING = "CROWDSALE_STATS_PENDING";
 export const CROWDSALE_STATS_FULFILLED = "CROWDSALE_STATS_FULFILLED";
 export const CROWDSALE_STATS_REJECTED = "CROWDSALE_STATS_REJECTED";
+export const TOKEN_STATS_PENDING = "TOKEN_STATS_PENDING";
+export const TOKEN_STATS_FULFILLED = "TOKEN_STATS_FULFILLED";
+export const TOKEN_STATS_REJECTED = "TOKEN_STATS_REJECTED";
 
-export const CROWDSALE_CONTRACT_INSTANTIATED = "CROWDSALE_CONTRACT_INSTANTIATED";
+export const CROWDSALE_CONTRACT_INSTANTIATED =
+  "CROWDSALE_CONTRACT_INSTANTIATED";
 
 export function instantiateCrowdsaleContract() {
   return (dispatch, getState) => {
     const web3 = getState().web3.web3Instance;
     const crowdsale = contract(BlitzCrowdsaleContract);
     crowdsale.setProvider(web3.currentProvider);
-    return crowdsale
-      .at("0x01270381a82c2f6db58f823dc864eeca3bb999d6")
-      .then(crowdsaleContract => {
-        dispatch({
-          type: CROWDSALE_CONTRACT_INSTANTIATED,
-          payload: crowdsaleContract
+    return (
+      crowdsale
+        //.deployed()
+        .at("0xb7e2462f5e36ba6cfffa9a6daf9ba6387a6a2525")
+        .then(crowdsaleContract => {
+          dispatch({
+            type: CROWDSALE_CONTRACT_INSTANTIATED,
+            payload: crowdsaleContract
+          });
+        })
+    );
+  };
+}
+
+export function instantiateTokenContract() {
+  return (dispatch, getState) => {
+    const web3 = getState().web3.web3Instance;
+    const token = contract(BlitzCrowdsaleTokenContract);
+    token.setProvider(web3.currentProvider);
+    return (
+      token
+        //.deployed()
+        .at("0x37b50b26900b6f3b5244e421cbb039df3fe0b39a")
+        .then(tokenContract => {
+          dispatch({
+            type: "TOKEN_CONTRACT_INSTANTIATED",
+            payload: tokenContract
+          });
+        })
+    );
+  };
+}
+
+
+
+export function getTokenStats() {
+  return async function(dispatch, getState) {
+    dispatch({ type: TOKEN_STATS_PENDING });
+    if (typeof getState().web3.token !== "undefined") {
+      let token = getState().web3.token;
+      let web3 = getState().web3.web3Instance;
+
+      let coinbase = web3.eth.coinbase;
+
+      //Get all the stats
+      try {
+        const balance = await token.balanceOf(coinbase);
+        const totalSupply = await token.totalSupply();
+        console.log('====================================');
+        console.log({
+          balance: balance,
+          totalSupply: totalSupply,
         });
+        console.log('====================================');
+        dispatch({
+          type: TOKEN_STATS_FULFILLED,
+          payload: {
+            balance: balance,
+            totalSupply: totalSupply,
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        dispatch({
+          type: TOKEN_STATS_REJECTED,
+          payload: {
+            error: "TOKEN_STATS_REJECTED"
+          }
+        });
+      }
+    } else {
+      console.error("did not work");
+      dispatch({
+        type: TOKEN_STATS_REJECTED,
+        payload: { error: "Web3 is not initialized." }
       });
+      console.error("Web3 is not initialized.");
+    }
   };
 }
 
@@ -28,7 +104,7 @@ export function getCrowdsaleStats() {
     dispatch({ type: CROWDSALE_STATS_PENDING });
     if (typeof getState().web3.crowdsale !== "undefined") {
       let crowdsale = getState().web3.crowdsale;
-        
+
       //Get all the stats
       try {
         const openingTime = await crowdsale.openingTime();
@@ -38,6 +114,7 @@ export function getCrowdsaleStats() {
         const goal = await crowdsale.goal();
         const cap = await crowdsale.cap();
         const weiRaised = await crowdsale.weiRaised();
+        const token = await crowdsale.token();
 
         dispatch({
           type: CROWDSALE_STATS_FULFILLED,
@@ -48,7 +125,8 @@ export function getCrowdsaleStats() {
             walletAddress: walletAddress,
             goal: goal,
             cap: cap,
-            weiRaised: weiRaised
+            weiRaised: weiRaised,
+            toke: token
           }
         });
       } catch (error) {
@@ -71,60 +149,29 @@ export function getCrowdsaleStats() {
   };
 }
 
-
 export function getNewStats() {
-    return  function(dispatch, getState) {
-      dispatch({ type: CROWDSALE_STATS_PENDING });
-      if (typeof getState().web3.crowdsale !== "undefined") {
-        let crowdsale = getState().web3.crowdsale;
-        var tokenPurchaseEvent = crowdsale.TokenPurchase();
+  return function(dispatch, getState) {
+    dispatch({ type: CROWDSALE_STATS_PENDING });
+    if (typeof getState().web3.crowdsale !== "undefined") {
+      let crowdsale = getState().web3.crowdsale;
+      var tokenPurchaseEvent = crowdsale.TokenPurchase();
 
-        //Get all the stats
-        try {
-            tokenPurchaseEvent.watch(async function(error, result){
-                if (!error)
-                    {
-                        const openingTime = await crowdsale.openingTime();
-                        const closingTime = await crowdsale.closingTime();
-                        const rate = await crowdsale.rate();
-                        const walletAddress = await crowdsale.wallet();
-                        const goal = await crowdsale.goal();
-                        const cap = await crowdsale.cap();
-                        const weiRaised = await crowdsale.weiRaised();
-                
-                        dispatch({
-                          type: CROWDSALE_STATS_FULFILLED,
-                          payload: {
-                            openingTime: openingTime,
-                            closingTime: closingTime,
-                            rate: rate,
-                            walletAddress: walletAddress,
-                            goal: goal,
-                            cap: cap,
-                            weiRaised: weiRaised
-                          }
-                        });
-                    } else {
-                        console.log(error);
-                    }
-            });
-         
-        } catch (error) {
-          console.error(error);
-          dispatch({
-            type: CROWDSALE_STATS_REJECTED,
-            payload: {
-              error: "CROWDSALE_STATS_REJECTED"
-            }
-          });
+      //Get all the stats
+      tokenPurchaseEvent.watch(async function(error, result) {
+        if (!error) {
+          getCrowdsaleStats()(dispatch, getState);
+          getTokenStats()(dispatch, getState);
+        } else {
+          console.log(error);
         }
-      } else {
-        console.error("did not work");
-        dispatch({
-          type: CROWDSALE_STATS_REJECTED,
-          payload: { error: "Web3 is not initialized." }
-        });
-        console.error("Web3 is not initialized.");
-      }
-    };
-  }
+      });
+    } else {
+      console.error("did not work");
+      dispatch({
+        type: CROWDSALE_STATS_REJECTED,
+        payload: { error: "Web3 is not initialized." }
+      });
+      console.error("Web3 is not initialized.");
+    }
+  };
+}
